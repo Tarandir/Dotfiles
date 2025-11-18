f76bc8aabc11d6e2551c80570772bb9f8d32c8c6
Tarandir <kuleshov.sergey@mail.ru>
2025-11-18 21:48:41 +0500

    Update youtube-handle-check.sh

--- waybar/youtube-handle-check.sh
+++ waybar/youtube-handle-check.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-HANDLE="Tarandir"
+HANDLE="DESIRED_HANDLE_HERE"
 URL="https://www.youtube.com/@${HANDLE}"
 
 status=$(curl -o /dev/null -s -w "%{http_code}" "$URL")
 
 if [ "$status" -eq 404 ]; then
     # Handle is available
-    echo "{\"text\": \"✔ YT AVAILABLE\", \"tooltip\": \"Handle Tarandir seems free\", \"class\": \"free\"}"
+    echo "{\"text\": \"✔ YT AVAILABLE\", \"tooltip\": \"Handle seems free\", \"class\": \"free\"}"
 else
     # Handle is taken
-    echo "{\"text\": \"YT:NO\", \"tooltip\": \"Handle Tarandir is taken\", \"class\": \"taken\"}"
+    echo "{\"text\": \"YT:NO\", \"tooltip\": \"Handle is taken\", \"class\": \"taken\"}"
 fi
 
 
