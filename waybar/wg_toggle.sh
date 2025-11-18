5be33e9b3626d81029fa82047d8ada9275b9494e
Tarandir <kuleshov.sergey@mail.ru>
2025-11-18 22:54:32 +0500

    Add files via upload

---
+++ waybar/wg_toggle.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+WG_INTERFACE="phone"
+
+# Check if interface exists and is up
+if ip link show "$WG_INTERFACE" &>/dev/null; then
+    # Turn it off
+    sudo wg-quick down "$WG_INTERFACE"
+else
+    # Turn it on
+    sudo wg-quick up "$WG_INTERFACE"
+fi
+
